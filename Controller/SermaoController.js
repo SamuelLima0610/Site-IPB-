d53ddcc0eaaa0fdbef00a12c08d4876c11ad0373
Samuel Vitorio Lima <34277130+SamuelLima0610@users.noreply.github.com>
2020-04-24 15:35:27 -0300

    Finalização do CRUD (Sermao)
    Finalização do CRUD de sermão.
    Ah o gerenciamento dos arquivos vinculados a cada sermão.
    Falta só o processo de validação para tratamento de erros.

--- Controller/SermaoController.js
+++ Controller/SermaoController.js
@@ -7,6 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser');
 //path
 const path = require('path');
+//fs (file)
+const fs = require('fs');
 //database sermon
 const Sermao = require('../Model/Sermao');
 
@@ -38,15 +40,8 @@ router.get('/sermao/adicionar', (req,res) => {
     res.render('sermons/create');
 });
 
-//Rota de listagem
-router.get('/sermao', (req,res) => {
-    Sermao.findAll({raw:true}).then(sermoes => {
-        res.render('sermons/read',{sermoes});
-    });
-});
-
 //Rota post para armazenamento
-router.post('/sermao/adicionar', upload.single("file"), (req,res) => {
+router.post('/sermao/criar', upload.single("file"), (req,res) => {
     let title = req.body.title;
     let abstract = req.body.abstract;
     let book = req.body.book;
@@ -59,11 +54,58 @@ router.post('/sermao/adicionar', upload.single("file"), (req,res) => {
         }else{
             Sermao.create({title,book,abstract,preacher,audio,link});
         }
-        res.redirect('/');
+        res.redirect('/admin/sermao');
     }else{
         Sermao.create({title,book,abstract,preacher})
         res.redirect('/');
     }
+});
+
+//Rota de listagem
+router.get('/sermao', (req,res) => {
+    Sermao.findAll({raw:true}).then(sermoes => {
+        res.render('sermons/read',{sermoes});
+    });
+});
+
+//Rota de editagem
+router.get('/sermao/editar/:id' , (req,res) => {
+    let id = req.params.id;
+    Sermao.findByPk(id).then(sermao => {
+        res.render('sermons/edit',{sermao});
+    });
+});
+
+//Rota para salvar os novos dados de um sermão
+router.post('/sermao/mudar' , (req,res) => {
+    let id = req.body.id;
+    let titleOld = req.body.titleOld;
+    let title = req.body.title;
+    let book = req.body.book;
+    let preacher = req.body.preacher;
+    let abstract = req.body.abstract;
+    let link = req.body.link;
+    var dirOld = `./public/audio/${titleOld}.mp3`
+    var dirNew = `./public/audio/${title}.mp3`
+    var audio = `/audio/${title}.mp3`
+    fs.rename(dirOld, dirNew, (err) => {
+        if (err) throw err;
+        Sermao.update({title,book,preacher,link,abstract,audio},{where:{id}}).then(()=> {
+            res.redirect('/admin/sermao');
+        });
+    });
+});
+
+router.post('/sermao/excluir', (req,res)=>{
+    let id = req.body.id;
+    let title = req.body.title;
+    var dir = `./public/audio/${title}.mp3`
+    fs.unlink(dir , err =>{
+        if (err) throw err;
+        Sermao.destroy({where:{id}}).then( () => {
+            res.redirect('/admin/sermao');
+        })
+    })
 });
 
 module.exports = router;
